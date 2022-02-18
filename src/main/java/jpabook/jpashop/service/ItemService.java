0d296345f4712b20cf2632369957d1e650b33bcc
kjy228 <zzucepace@gmail.com>
2022-02-18 23:31:26 +0900

    test

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class ItemService {
     private final ItemRepository itemRepository;
     // itemRepository 위임해서 서비스를 구현했는데 복잡하지 않을 경우는 repository 직접 써도 될듯.
-    
+
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
